f463404fadb95168263e163c647f47a577dc27dc
Meor HZM <notmeorhazim@gmail.com>
2025-07-10 22:07:56 +0800

    Added search functionality (unfinished)

--- projectEDP/projectEDP/AdminHistory.cs
+++ projectEDP/projectEDP/AdminHistory.cs
@@ -25,8 +25,55 @@ namespace projectEDP
 
         private void AdminHistory_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'eDP_DatabaseDataSet.Users' table. You can move, or remove it, as needed.
-            this.usersTableAdapter.Fill(this.eDP_DatabaseDataSet.Users);
+            // TODO: This line of code loads data into the 'eDP_DatabaseDataSet2.Package' table. You can move, or remove it, as needed.
+            this.packageTableAdapter.Fill(this.eDP_DatabaseDataSet2.Package);
+            // TODO: This line of code loads data into the 'eDP_DatabaseDataSet1.Discount' table. You can move, or remove it, as needed.
+            this.discountTableAdapter.Fill(this.eDP_DatabaseDataSet1.Discount);
+            // TODO: This line of code loads data into the 'eDP_DatabaseDataSet.History' table. You can move, or remove it, as needed.
+            this.historyTableAdapter.Fill(this.eDP_DatabaseDataSet.History);
+        }
+
+        private void ApplyFilters()
+{
+    string query = "SELECT * FROM History WHERE 1=1"; // Always true, allows dynamic appending
+
+    if (!string.IsNullOrWhiteSpace(discountName.Text))
+    {
+        query += " AND Discount_Name LIKE @DiscountName";
+    }
+
+    if (pkgName.SelectedIndex != -1)
+    {
+        query += " AND pkg_name = @PkgName";
+    }
+
+    if (!string.IsNullOrWhiteSpace(userName.Text))
+    {
+        query += " AND Name LIKE @Username";
+    }
+
+    if (dateFilter.Checked) // Add a checkbox if you want to allow date filtering optionally
+    {
+        query += " AND CAST(date AS DATE) = @BookingDate";
+    }
+
+    using (SqlConnection conn = new SqlConnection("your_connection_string"))
+    using (SqlCommand cmd = new SqlCommand(query, conn))
+    {
+        if (!string.IsNullOrWhiteSpace(discountName.Text))
+        {
+            cmd.Parameters.AddWithValue("@DiscountName", "%" + discountName.Text + "%");
+        }
+
+        if (pkgName.SelectedIndex != -1)
+        {
+            cmd.Parameters.AddWithValue("@PkgName", pkgName.SelectedItem.ToString());
+        }
+
+        if (!string.IsNullOrWhiteSpace(userName.Text))
+        {
+            cmd.Parameters.AddWithValue("@Username", "%" + userName.Text + "%");
+        }
             
         }
 
